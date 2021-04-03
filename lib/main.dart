664f3d7037c1607b8466ec722a1f35487e544ac4
ashiq1211 <ashiqknt@gmail.com>
2021-04-03 11:26:26 +0530

    download

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:project/Pages/cam_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+await FlutterDownloader.initialize(
+  debug: true // optional: set false to disable printing logs to console
+);
   runApp(MyApp());
 }
 
